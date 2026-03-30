594b21f04e6c2ac7d1beb21f46ea86575663b7ba
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-30 18:01:32 +0200

    Update translation, add support website URL

--- src/upload/admin/controller/extension/feed/ps_product_data_feed.php
+++ src/upload/admin/controller/extension/feed/ps_product_data_feed.php
@@ -7,9 +7,14 @@ class ControllerExtensionFeedPSProductDataFeed extends Controller
     const EXTENSION_EMAIL = 'support@playfulsparkle.com';
 
     /**
-     * @var string The documentation URL for the extension.
+     * @var string The URL to the support website.
      */
-    const EXTENSION_DOC = 'https://github.com/playfulsparkle/oc3_product_data_feed.git';
+    const SUPPORT_URL = 'https://support.playfulsparkle.com';
+
+    /**
+     * @var string The GitHub repository URL of the extension.
+     */
+    const GITHUB_REPO_URL = 'https://github.com/playfulsparkle/oc3_product_data_feed';
 
     private $error = array();
 
@@ -209,7 +214,7 @@ class ControllerExtensionFeedPSProductDataFeed extends Controller
 
         $data['backup_gbc2c'] = $this->url->link('extension/feed/ps_product_data_feed/backup_gbc2c', 'user_token=' . $this->session->data['user_token'] . '&store_id=' . $store_id, true);
 
-        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::EXTENSION_EMAIL, self::EXTENSION_EMAIL, self::EXTENSION_DOC);
+        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::SUPPORT_URL, self::GITHUB_REPO_URL, self::EXTENSION_EMAIL);
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
